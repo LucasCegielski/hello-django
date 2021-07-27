5cec358ac4399e32ace10937878acdac70dc5169
LucasCegielski <lucas.cegielski@yahoo.ie>
2021-07-27 09:48:17 +0000

    commit

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
-
+from .models import Item
 # Create your views here.
-def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
 
+
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'item': items
+    }
+    return render(request, 'todo/todo_list.html', context)
